b3ce518e523cc446714191cc4d0dafda4e5b432e
Elektromannen <Ottarmr@stud.ntnu.no>
2025-10-24 22:58:33 +0200

    added the ability to change serial baudrate and port, + the ability to show the current settings in the settings menu

--- Data_processing/Data_processor.py
+++ Data_processing/Data_processor.py
@@ -6,6 +6,9 @@ from pynput import keyboard
 from pathlib import Path
 import serial.tools.list_ports
 
+
+serial_port = ""
+serial_baudrate = 9600
 reading_toggle = False
 end = False
 
@@ -24,6 +27,37 @@ def on_press(key):
 
 #Init keyboard listner
 key_watcher = keyboard.Listener(on_press=on_press)
+
+#Seial port checker
+def serial_port_check():
+    print("Placeholder code")
+    
+#Manages filecreation and data reading/writing to file
+def data_record(filename):
+    global end
+    #Checking if file exists and creates file if not
+    folder = Path("Data")
+    folder.mkdir(exist_ok=True)
+    filepath = Path(f"{folder}/{filename}.csv")
+    if not filepath.exists():
+        print(f"creating {filename}.csv")
+        with open(filepath, mode="w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Time", "Value"])
+        print(f"File created at {filepath}")
+    else:
+        print("File allready exists")
+
+
+
+    #Data recording can begin
+    print("\nTo start and stop data recording use spacebar \nUse Escape key to end data recording")
+
+    
+    key_watcher.start()
+    if(end):
+        key_watcher.stop()
+
 
 #Menu for some graphic interface for user
 def print_Menu():
@@ -53,56 +87,49 @@ def print_Menu():
                 print("Quitting program")
                 break
             case _:
-                print("not a choice")
+                pass
 
 
-
+#Menu for handling serial changes
 def settings_menu():
+    global serial_port, serial_baudrate
     while True:
         print("--------Settings-------")
         print("|1. Change Serial Port|")
-        print("|2. Plot data         |")
+        print("|2. Change Baud rate  |")
+        print("|3. Show Serial info  |")
         print("|0. Back to main menu |")
         print("-----------------------")
         choice = int(input("What would you like to do?\n"))
         match choice:
-            case 1:
-                print("Change serial port")
-            case 2:
-                print("Something")
-            case 0:
+            case 1: #Changing seral port
+                print("Available serial ports:")
+                ports = serial.tools.list_ports.comports()
+                for i, port in enumerate(ports, start=1):
+                    if((port.description != "n/a") and (port.hwid != "n/a")):
+                        print(f"{i}:\nDevice: {port.device}")
+                        print(f"Description: {port.description}")
+                        print(f"Hardware ID: {port.hwid}")
+                        print("-" * 20)
+
+                port_num = int(input("Which device is your device?\n"))
+                serial_port = ports[port_num].device
+                print(f"New device is {serial_port}")
+
+            case 2:#Changing baudrate
+                print("-" * 20)
+                serial_baudrate = int(input("Please write the new baudrate\n(Standards are 9600 or 115200)\n"))
+                print(f"New baud rate is {serial_baudrate}")
+            case 3:
+                print("-" * 20)
+                print("Serial info:")
+                print(f"Serial port: {serial_port}\nBaudrate: {serial_baudrate}")
+            case 0:#Back to main menu
                 break
-
-
-    
-#Manages filecreation and data reading/writing to file
-def data_record(filename):
-    global end
-    #Checking if file exists and creates file if not
-    folder = Path("Data")
-    folder.mkdir(exist_ok=True)
-    filepath = Path(f"{folder}/{filename}.csv")
-    if not filepath.exists():
-        print(f"creating {filename}.csv")
-        with open(filepath, mode="w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["Time", "Value"])
-        print(f"File created at {filepath}")
-    else:
-        print("File allready exists")
-
-    #Data recording can begin
-    print("\nTo start and stop data recording use spacebar \nUse Escape key to end data recording")
-
-    
-    key_watcher.start()
-    if(end):
-        key_watcher.stop()
-
+            case _: #Not a choice
+                pass
+            
 
 
 #Starts program
 print_Menu()
-
-
-    
