f9ea0fec576c37394e5acafd286253682b369ab2
Elektromannen <Ottarmr@stud.ntnu.no>
2025-10-30 20:40:30 +0100

    Start of sensor code

--- Sensor/src/main.cpp
+++ Sensor/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//Pin asignments
+int sensor_select_bttn = 2;
+int data_sending_bttn = 4;
+int data_led = 0;
+
+bool data_toggle = false;
+int sensor_choice = 0;
+//Function definition
+void US_sensor_read(int pin);
+void IR_sensor_read(int pin);
+void TOF_sensor_read(int pin);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  attachInterrupt(sensor_select_bttn, ISR, FALLING);
+  attachInterrupt(data_sending_bttn, datastreamer_toggle, FALLING);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+//Interupt functions
+void IRAM_ATTR datastreamer_toggle(){
+  data_toggle = !data_toggle;
+  if (data_toggle == true){
+    data_led = HIGH;
+  }
+  else
+    data_led = LOW;
+}
+
+void IRAM_ATTR sensor_select(){
+  
 }
